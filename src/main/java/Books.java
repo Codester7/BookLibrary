697c7d244eb3acd9296dde137802a9d1fe07442d
vladislavpopov <vd.popov7@gmail.com>
2022-07-29 16:58:07 +0300

    Правки по замечаниям

--- src/main/java/Books.java
+++ src/main/java/Books.java
@@ -1,10 +1,10 @@
 public class Books {
     public static void main(String[] args) {
 
-        Library Book1 = new Library("Корабль Дураков", "Сибастиан Брант", 278, "Поэзия");
-        Library Book2 = new Library("Повелитель мух", "Уильям Голдинг", 318, "Художественная литературва");
-        Library Book3 = new Library("История России", "А.С. Орлов", 544, "Учебная литература");
-        Book1.hellodiscription();
-        Book1.pages();
+        Library book1 = new Library("Корабль Дураков", "Сибастиан Брант", 278, "Поэзия");
+        Library book2 = new Library("Повелитель мух", "Уильям Голдинг", 318, "Художественная литературва");
+        Library book3 = new Library("История России", "А.С. Орлов", 544, "Учебная литература");
+        book1.printinfo();
+        book1.countpages();
     }
 }
--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 public class Library {
     int allPages;
-    int pagesCount;
     String name;
     String author;
     String type;
-    String Status;
+
 
     public Library(String name, String author, int allPages, String type) {
         this.allPages = allPages;
@@ -14,13 +13,14 @@ public class Library {
         this.author = author;
         this.type = type;
     }
-        void pages () {                                       // Считает сколько страниц осталось до конца книги.
+
+    void countpages() {                                       // Считает сколько страниц осталось до конца книги.
 
         Scanner in = new Scanner(System.in);
         System.out.println("На какой вы странице?");
         int a = in.nextInt();
 
-        if (a < allPages && a>0) {
+        if (a < allPages && a > 0) {
             int result = allPages - a;
             System.out.println("До конца этой книги осталось" + " " + result);
         } else if (a >= allPages) {
@@ -29,12 +29,12 @@ public class Library {
             System.out.println("Введенные данные некорректны.");
     }
 
-     void hellodiscription(){                                // Приветсвенное сообщение при выборе книги
-         System.out.println("Приветсвую!");
-         System.out.println("Вы читает следующую книгу:");
-         System.out.println("Название:" +" "+ name + " ");
-         System.out.println("Автор:" +" "+ author + " ");
-         System.out.println("Тип:" +" "+ type + " ");
+    void printinfo() {                                // Приветсвенное сообщение при выборе книги
+        System.out.println("Приветсвую!");
+        System.out.println("Вы читает следующую книгу:");
+        System.out.println("Название:" + " " + name + " ");
+        System.out.println("Автор:" + " " + author + " ");
+        System.out.println("Тип:" + " " + type + " ");
     }
 
 }
